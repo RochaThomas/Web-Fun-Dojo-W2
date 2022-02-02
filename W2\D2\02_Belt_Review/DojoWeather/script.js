e9fd6453728b529a508cd134251c581f42e8c75f
Tommy Rocha <tomrocha99@gmail.com>
2022-02-01 19:16:17 -0800

    dojo weather initial commit, need to convert temps

---
+++ W2\D2\02_Belt_Review/DojoWeather/script.js
@@ -0,0 +1,9 @@
+
+function navigatingTo(element){
+    alert("Navigating to the " + element.innerText + " weather page...");
+}
+
+function accepted(){
+    var cookie = document.querySelector(".cookie");
+    cookie.remove();
+}
